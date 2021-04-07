c620801ef6745075600114f7f67a85cd43cddf7a
Ruslan Sharipov <rusharipov2004@gmail.com>
2021-04-07 17:54:02 +0500

    Added media

--- src/script/index.js
+++ src/script/index.js
@@ -55,3 +55,16 @@ const svg = {
 document.addEventListener("DOMContentLoaded", function(e){
 	svg.load();
 });
+
+$(function () {
+	var link = $('.m-menu-link');
+	var close = $('.close-menu');
+	var menu = $('.m-menu');
+	link.on('click', function (event) {
+	  event.preventDefault();
+	  menu.toggleClass('m-menu__active');
+	});
+	close.on('click', function (event) {
+	  menu.toggleClass('m-menu__active');
+	});
+  });
